fcb904bd3cbd50b8f61bbd70fe5738cce85ec965
Tatva Kamdar <kamdartatv1@gmail.com>
2023-05-31 18:30:52 +0530

    Create Contest 90% done

--- frontend/src/components/ContestComponents/CreateContestCalculationFormula.jsx
+++ frontend/src/components/ContestComponents/CreateContestCalculationFormula.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const CreateContestCalculationFormula = () => {
-  return (
-<section className="w-11/12 mx-auto">
-    
-</section>
-  )
-}
+  const [testCasesWeightage, setTestCasesWeightage] = useState(100);
+  const [timeSpentWeightage, setTimeSpentWeightage] = useState(0);
 
-export default CreateContestCalculationFormula
+  const handleTestCasesWeightageChange = (e) => {
+    if (e.target.value > 100 || e.target.value < 0) {
+      return;
+    }
+    setTestCasesWeightage(e.target.value);
+    setTimeSpentWeightage(100 - e.target.value);
+  };
+
+  const handleTimeSpentWeightageChange = (e) => {
+    if (e.target.value > 100 || e.target.value < 0) {
+      return;
+    }
+    setTestCasesWeightage(100 - e.target.value);
+    setTimeSpentWeightage(e.target.value);
+  };
+
+  return (
+    <section className="w-full flex flex-col justify-evenly mx-auto">
+      <p className="font-mono font-semibold text-3xl underline p-5">
+        Calculation Formula
+      </p>
+      <div className="flex">
+        <div className="w-full flex justify-center place-items-center gap-x-3 p-4">
+          <label
+            htmlFor="testCasesWeightage"
+            className="font-medium text-xl text-gray-800"
+          >
+            Total Test Cases Passed Weightage
+          </label>
+          <input
+            type="number"
+            id="testCasesWeightage"
+            value={testCasesWeightage}
+            onChange={handleTestCasesWeightageChange}
+            min={0}
+            max={100}
+            className="w-20 h-10 px-4 py-2 border rounded-md font-semibold text-lg"
+          />
+        </div>
+
+        <div className="w-full flex justify-center place-items-center gap-x-3 p-4">
+          <label
+            htmlFor="timeSpentWeightage"
+            className="font-medium text-xl text-gray-800"
+          >
+            Time Spent Per Question Weightage
+          </label>
+          <input
+            type="number"
+            id="timeSpentWeightage"
+            value={timeSpentWeightage}
+            onChange={handleTimeSpentWeightageChange}
+            min={0}
+            max
+            className="w-20 h-10 px-4 py-2 border rounded-md font-semibold text-lg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CreateContestCalculationFormula;
--- frontend/src/components/ContestComponents/CreateContestNavbar.jsx
+++ frontend/src/components/ContestComponents/CreateContestNavbar.jsx
@@ -4,60 +4,51 @@ const CreateChallangeNavbar = ({ selection, setSelection }) => {
   const DETAILS = "DETAILS";
   const QUESTIONS = "QUESTIONS";
   const CALCULATION_FORMULA = "CALCULATION_FORMULA";
-  const MODERATORS = "MODERATORS";
   const NOTIFICATIONS = "NOTIFICATIONS";
   const ADD_PARTICIPANTS = "ADD_PARTICIPANTS";
 
   return (
-    <section className="grid grid-cols-4 lg:grid-cols-6 place-items-center gap-y-4 bg-gray-50 border border-b-0 p-4 pb-0 text-gray-500">
+    <section className="grid grid-cols-4 lg:grid-cols-5 place-items-center gap-y-4 bg-gray-50 border border-b-0 p-4 pb-0 text-gray-500">
       <p
-        className={`h-14 w-36 flex justify-center place-items-center mx-auto text-center cursor-pointer select-none ${
-          selection === DETAILS && "border border-b-0 border-gray-600 bg-white"
+        className={`h-14 w-36 flex justify-center place-items-center mx-auto text-center cursor-pointer select-none rounded-t-md ${
+          selection === DETAILS &&
+          "border-2 border-b-0 border-gray-200 bg-white"
         }`}
         onClick={() => setSelection(DETAILS)}
       >
         Details
       </p>
       <p
-        className={`h-14 w-36 flex justify-center place-items-center mx-auto text-center cursor-pointer select-none ${
+        className={`h-14 w-36 flex justify-center place-items-center mx-auto text-center cursor-pointer select-none rounded-t-md ${
           selection === QUESTIONS &&
-          "border border-b-0 border-gray-600 bg-white"
+          "border-2 border-b-0 border-gray-200 bg-white"
         }`}
         onClick={() => setSelection(QUESTIONS)}
       >
         Questions
       </p>
       <p
-        className={`h-14 w-36 flex justify-center place-items-center mx-auto text-center cursor-pointer select-none ${
+        className={`h-14 w-36 flex justify-center place-items-center mx-auto text-center cursor-pointer select-none rounded-t-md ${
           selection === ADD_PARTICIPANTS &&
-          "border border-b-0 border-gray-600 bg-white"
+          "border-2 border-b-0 border-gray-200 bg-white"
         }`}
         onClick={() => setSelection(ADD_PARTICIPANTS)}
       >
         Add Participants
       </p>
       <p
-        className={`h-14 w-36 flex justify-center place-items-center mx-auto text-center cursor-pointer select-none ${
+        className={`h-14 w-36 flex justify-center place-items-center mx-auto text-center cursor-pointer select-none rounded-t-md ${
           selection === CALCULATION_FORMULA &&
-          "border border-b-0 border-gray-600 bg-white"
+          "border-2 border-b-0 border-gray-200 bg-white"
         }`}
         onClick={() => setSelection(CALCULATION_FORMULA)}
       >
         Calculation Formula
       </p>
       <p
-        className={`h-14 w-36 flex justify-center place-items-center mx-auto text-center cursor-pointer select-none ${
-          selection === MODERATORS &&
-          "border border-b-0 border-gray-600 bg-white"
-        }`}
-        onClick={() => setSelection(MODERATORS)}
-      >
-        Moderators
-      </p>
-      <p
-        className={`h-14 w-36 flex justify-center place-items-center mx-auto text-center cursor-pointer select-none ${
+        className={`h-14 w-36 flex justify-center place-items-center mx-auto text-center cursor-pointer select-none rounded-t-md ${
           selection === NOTIFICATIONS &&
-          "border border-b-0 border-gray-600 bg-white"
+          "border-2 border-b-0 border-gray-200 bg-white"
         }`}
         onClick={() => setSelection(NOTIFICATIONS)}
       >
