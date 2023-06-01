c62163fcf74c191ed417da4daff42e5e09d75f6b
Tatva Kamdar <kamdartatv1@gmail.com>
2023-06-02 02:15:50 +0530

    mr

--- frontend/src/pages/ManageContest.jsx
+++ frontend/src/pages/ManageContest.jsx
@@ -3,12 +3,9 @@ import AddQuestions from "../components/ContestComponents/AddQuestions";
 import CalculationFormula from "../components/ContestComponents/CalculationFormula";
 import Details from "../components/ContestComponents/Details";
 import ContestNavbar from "../components/ContestComponents/ContestNavbar";
-<<<<<<< HEAD
 import Leaderboard from "../components/ContestComponents/Leaderboard";
-=======
 import CreateGroup from "./CreateGroup";
 import AddParticipants from "./AddParticipants";
->>>>>>> a63d223fd5e88019be01e33fa37401a947d04025
 
 const CreateContest = () => {
   const DETAILS = "DETAILS";
@@ -50,12 +47,9 @@ const CreateContest = () => {
       {selection === DETAILS && <Details />}
       {selection === QUESTIONS && <AddQuestions />}
       {selection === CALCULATION_FORMULA && <CalculationFormula />}
-<<<<<<< HEAD
 
       {selection === LEADERBOARD && <Leaderboard />}
-=======
       {selection === ADD_PARTICIPANTS && <AddParticipants contestId={x} />}
->>>>>>> a63d223fd5e88019be01e33fa37401a947d04025
     </div>
   );
 };
